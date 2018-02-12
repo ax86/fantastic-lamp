83cf0910f9ff0e8dd10e2133d854ff7fbd4484e2
Aleksandr Filippov <aleksandr@filippov.mobi>
2018-02-12 17:21:19 +0300

    Initial Commit

---
+++ DigitalSkills/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DigitalSkills
+//
+//  Created by Aleksandr Filippov on 12.02.2018.
+//  Copyright © 2018 Aleksandr Filippov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
